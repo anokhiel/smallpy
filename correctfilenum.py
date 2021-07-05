49933095afba0ed4317be77ff4253e9b9c99d657
Alexander Popov <alexandpopov@yandex.ru>
2021-07-05 15:18:05 +0300

    more general

--- correctfilenum.py
+++ correctfilenum.py
@@ -1,12 +1,24 @@
 import os
 pref="" #add prefix here
+ 
+def getmax(fs):
+    n=0
+    for f in fs:
+        s=f.split('.')
+        so=s[0][len(pref):]
+        if(len(so)>n):
+            n=len(so)
+    return n
+
 cdir=os.getcwd()
 for r, d, fs in os.walk(cdir):
+    m=getmax(fs)
     for f in fs:
         af=cdir+'\\'+f
         s=f.split('.')
         so=s[0][len(pref):]
         k=len(so)
+        m=getmax 
         if(k<2 and s[1]!='py'):
             print(af)
-            os.rename(af,cdir+'\\'+pref+'0'+so+'.'+s[1])
+            os.rename(af,cdir+'\\'+pref+'0'*m+so+'.'+s[1])
